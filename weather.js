6cad363e024815ed1cfb54148471dafb90833493
onek19 <lwk9819@naver.com>
2020-06-05 23:57:18 +0900

    댓글창

---
+++ weather.js
@@ -0,0 +1,60 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "3e6c1283da3795221cc283c08589b899"; 
+
+const COORDS = "coords";
+
+function getWeather(lat, long) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+  )
+
+    .then(function(response) {
+      return response.json();
+			
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const name = json.name;
+      weather.innerText = `오늘의 온도 : ${temperature}℃ and 장소 : ${name}`
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cannot access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
